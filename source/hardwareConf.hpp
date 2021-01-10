54ed77090c512cc366a998897ab4149f9315c84b
Alexandre Bustico <alexandre.bustico@enac.fr>
2021-01-10 22:49:05 +0100

    fix I²C timinggr was for stm32f7

--- source/hardwareConf.hpp
+++ source/hardwareConf.hpp
@@ -100,20 +100,20 @@ static inline    stm32_tim_t * const ENCODER_F1 = CONCAT(STM32_TIM, BOUTON_F1_IN
 static inline    stm32_tim_t * const ENCODER_F2 = CONCAT(STM32_TIM, BOUTON_F2_IN_A_TIM);
 
 static constexpr I2CDriver &I2C_FRAM = CONCAT(I2CD, MEM_SCL_I2C);
-static constexpr uint32_t I2C_FAST_400KHZ_DNF3_R200NS_F50NS_PCLK54MHZ_TIMINGR = 0x10800C27;
-static constexpr uint32_t I2C_FAST_1MHZ_DNF3_R100NS_F50NS_PCLK54MHZ_TIMINGR  = 0x00800617;
+static constexpr uint32_t I2C_FAST_400KHZ_DNF3_R200NS_F50NS_PCLK80MHZ_TIMINGR = 0x10B0133C;
+static constexpr uint32_t I2C_FASTPLUS_1MHZ_DNF3_R100NS_F50NS_PCLK80MHZ_TIMINGR  = 0x00B00B25;
 static constexpr uint32_t stm32_cr1_dnf(const uint32_t n) {
   return (n & 0x0f) << 8;
 }
 
 static constexpr I2CConfig i2ccfg_400 = {
-  .timingr = I2C_FAST_400KHZ_DNF3_R200NS_F50NS_PCLK54MHZ_TIMINGR, // Refer to the STM32F7 reference manual
+  .timingr = I2C_FAST_400KHZ_DNF3_R200NS_F50NS_PCLK80MHZ_TIMINGR, // Refer to the STM32F7 reference manual
   .cr1 =  stm32_cr1_dnf(3U), // Digital noise filter activated (timingr should be aware of that)
   .cr2 = 0 // Only the ADD10 bit can eventually be specified here (10-bit addressing mode)
 } ;
 
 static constexpr I2CConfig i2ccfg_1000 = {
-  .timingr = I2C_FAST_1MHZ_DNF3_R100NS_F50NS_PCLK54MHZ_TIMINGR, // Refer to the STM32F7 reference manual
+  .timingr = I2C_FASTPLUS_1MHZ_DNF3_R100NS_F50NS_PCLK80MHZ_TIMINGR, // Refer to the STM32F7 reference manual
   .cr1 = stm32_cr1_dnf(3U), // Digital noise filter activated (timingr should be aware of that)
   .cr2 = 0 // Only the ADD10 bit can eventually be specified here (10-bit addressing mode)
 } ;
