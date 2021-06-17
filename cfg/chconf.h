31073ef9ce1e88fcae100cd916562b03d8e7e3ff
Alexandre Bustico <alexandre.bustico@enac.fr>
2021-06-17 19:59:34 +0200

    use tickless mode inconditionnaly

--- cfg/chconf.h
+++ cfg/chconf.h
@@ -72,7 +72,7 @@
  *          setting also defines the system tick time unit.
  */
 #if !defined(CH_CFG_ST_FREQUENCY)
-#define CH_CFG_ST_FREQUENCY                 2000
+#define CH_CFG_ST_FREQUENCY                 10000
 #endif
 
 /**
@@ -100,7 +100,7 @@
  *          this value.
  */
 #if !defined(CH_CFG_ST_TIMEDELTA)
-#define CH_CFG_ST_TIMEDELTA                 0
+#define CH_CFG_ST_TIMEDELTA                 2
 #endif
 
 /** @} */
@@ -125,7 +125,7 @@
  *          must be set to zero in that case.
  */
 #if !defined(CH_CFG_TIME_QUANTUM)
-#define CH_CFG_TIME_QUANTUM                 2
+#define CH_CFG_TIME_QUANTUM                 0
 #endif
 
 /**
